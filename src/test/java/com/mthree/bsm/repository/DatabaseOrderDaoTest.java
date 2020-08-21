376e0850ee193aac201f751484fb261314d427f2
Thomas Barton <verites95@gmail.com>
2020-08-21 23:50:17 +0100

    tests

--- src/test/java/com/mthree/bsm/repository/DatabaseOrderDaoTest.java
+++ src/test/java/com/mthree/bsm/repository/DatabaseOrderDaoTest.java
@@ -5,16 +5,22 @@
  */
 package com.mthree.bsm.repository;
 
-import com.mthree.bsm.entity.Order;
-import com.mthree.bsm.entity.OrderStatus;
+import static com.mthree.bsm.entity.OrderStatus.PENDING;
+import com.mthree.bsm.entity.Stock;
+import com.mthree.bsm.entity.User;
+import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  *
@@ -24,6 +30,15 @@ public class DatabaseOrderDaoTest {
     
     public DatabaseOrderDaoTest() {
     }
+    
+    @Autowired
+    OrderDao orderDao;
+    
+    @Autowired
+    UserDao userDao;
+    
+    @Autowired
+    StockDao stockDao;
     
     @BeforeAll
     public static void setUpClass() {
@@ -35,6 +50,7 @@ public class DatabaseOrderDaoTest {
     
     @BeforeEach
     public void setUp() {
+        orderDao.deleteOrders();
     }
     
     @AfterEach
@@ -45,7 +61,87 @@ public class DatabaseOrderDaoTest {
      * Test of getOrders method, of class OrderRepository.
      */
     @Test
-    public void testGetOrders() {
+    public void testAddGetOrders() {
+        
+        Order invalidOrder1 = new Order();
+        assertThrows(InvalidEntityException.class, orderDao.addOrder(invalidOrder1));
+        
+        Stock tesla = new Stock();
+        tesla.setSymbol("TSLA");
+        tesla.setExchange("NASDAQ");
+        stockDao.addStock(tesla);
+        invalidOrder1.setStock(tesla);
+        
+        invalidOrder1.setLotSize(100);
+        invalidOrder1.setRemainingSize(100);
+        invalidOrder1.setIsBuy(true);
+        invalidOrder1.setOrderStatus(PENDING);
+        
+        LocalDateTime ldt = LocalDateTime.now();
+        invalidOrder1.setCreationDate(ldt);
+        
+        assertThrows(InvalidEntityException.class, orderDao.addOrder(invalidOrder1));
+ 
+        User tom = new User();
+        tom.setUsername("TomB");
+        tom.setDeleted(false);
+        tom = userDao.addUser(tom);
+        invalidOrder1.setUser(tom);
+        
+        invalidOrder1.setStock(null);
+        assertThrows(InvalidEntityException.class, orderDao.addOrder(invalidOrder1));
+        
+        invaliorder1.setStock(tesla);
+        invalidOrder1.setLotSize(-1);
+        assertThrows(InvalidEntityException.class, orderDao.addOrder(invalidOrder1));
+        invalidOrder1.setLotSize(100001);
+        assertThrows(InvalidEntityException.class, orderDao.addOrder(invalidOrder1));
+        
+        invalidOrder1.setLotSize(100);
+        invalidOrder1.setRemainingSize(-1);
+        assertThrows(InvalidEntityException.class, orderDao.addOrder(invalidOrder1));
+        invalidOrder1.setRemainingSize(100001);
+        assertThrows(InvalidEntityException.class, orderDao.addOrder(invalidOrder1));
+        
+        invalidOrder1.setRemainingSize(100);
+        invalidOrder1.setIsBuy(null);
+        assertThrows(InvalidEntityException.class, orderDao.addOrder(invalidOrder1));
+        
+        invalidOrder1.setIsBuy(true);
+        invalidOrder1.setOrderStatus(null);
+        assertThrows(InvalidEntityException.class, orderDao.addOrder(invalidOrder1));
+        
+        invalidOrder1.setOrderStatus(PENDING);
+        invalidOrder1.setCreationDate(null);
+        assertThrows(InvalidEntityException.class, orderDao.addOrder(invalidOrder1));
+        // may need to adjust number of decimal places
+        LocalDateTime futureLDT = LocalDateTime.of(2019, 03, 28, 14, 33, 48, 123456789);
+        invalidOrder1.setCreationTime(futureLDT);
+        assertThrows(InvalidEntityException.class, orderDao.addOrder(invalidOrder1));
+        
+        invalidOrder1.setCreationTime(ldt);
+        
+        Order validOrder1 = invalidOrder1;
+        validOrder1 = orderDao.addOrder(validOrder1);
+        
+        Order validOrder2 = invalidOrder1;
+        validOrder2.setIsBuy(false);
+        validOrder2 = orderDao.addOrder(validOrder2);
+        
+        Order validOrder3 = invalidOrder1;
+        validOrder3.setPrice(150);
+        
+        List<Order> orders = orderDao.getOrders();
+        
+        assertEquals(orders.size(), 2);
+        assertTrue(orders.contains(validOrder1) && orders.contains(validOrder2));
+        assertFalse(orders.contains(validOrder3));
+        
+        validOrder3 = orderDao.addOdrer(validOrder3);
+        orders = orderDao.getOrders();
+        
+        assertEquals(orders.size(), 3);
+        assertTrue(orders.contains(validOrder1) && orders.contains(validOrder2) && orders.contains(validOrder3));
     }
 
     /**
@@ -53,6 +149,42 @@ public class DatabaseOrderDaoTest {
      */
     @Test
     public void testGetOrdersBySide() {
+        
+        Order Order = new Order();
+        
+        User tom = new User();
+        tom.setUsername("TomB");
+        tom.setDeleted(false);
+        tom = userDao.addUser(tom);
+        order.setUser(tom);
+        
+        Stock tesla = new Stock();
+        tesla.setSymbol("TSLA");
+        tesla.setExchange("NASDAQ");
+        stockDao.addStock(tesla);
+        order.setStock(tesla);
+        
+        order.setLotSize(100);
+        order.setRemainingSize(100);
+        order.setIsBuy(true);
+        order.setOrderStatus(PENDING);
+        LocalDateTime ldt = LocalDateTime.now();
+        order.setCreationDate(ldt);
+        
+        Order buyOrder = orderDao.addOrder(order);
+        
+        order.setIsBuy(false);
+        Order sellOrder = orderDao.addOrder(order);
+ 
+        List<Order> orders = getOrdersBySide(true);
+        
+        assertEquals(orders.size(), 1);
+        assertTrue(orders.contains(buyOrder));
+                
+        orders = getOrdersBySide(false);
+        
+        assertEquals(orders.size(), 1);
+        assertTrue(orders.contains(sellOrder));
     }
 
     /**
