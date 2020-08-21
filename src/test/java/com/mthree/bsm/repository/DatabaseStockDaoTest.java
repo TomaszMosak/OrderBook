847fb649c964f1a7ec04af063f65b5475dd8c6c6
Thomas Barton <verites95@gmail.com>
2020-08-21 15:40:27 +0100

    Establishing testing

---
+++ src/test/java/com/mthree/bsm/repository/DatabaseStockDaoTest.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mthree.bsm.repository;
+
+import com.mthree.bsm.entity.Stock;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author tombarton
+ */
+public class DatabaseStockDaoTest {
+    
+    public DatabaseStockDaoTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getStocks method, of class StockRepository.
+     */
+    @Test
+    public void testGetStocks() {
+    }
+
+    
+}
