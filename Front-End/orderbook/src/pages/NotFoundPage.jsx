45520e6d879ff8301d514b97f6df782ac7555909
Tomasz Mosak <tomekmosak1@gmail.com>
2020-08-25 16:49:47 +0100

    AutoRedirect on 404 Error

--- Front-End/orderbook/src/pages/NotFoundPage.jsx
+++ Front-End/orderbook/src/pages/NotFoundPage.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, {Component} from "react";
+import { Redirect} from "react-router-dom";
 
-const NotFoundPage = () => {
-    return (
-        <div>
-            <h3>ERROR: 404 Not Found!</h3>
-        </div>
-    )
+class NotFoundPage extends Component{
+    state = {
+        redirect: false,
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                redirect: true,
+            })
+        }, 2000)
+    }
+
+    render() {
+        if (this.state.redirect) {
+            return (
+                <Redirect to={'/'} />
+            )
+        }
+
+        return (
+            <div>
+                <h3 className="text-center">ERROR 404: Not Found</h3>
+                <h5 className="text-center">Redirecting to homepage in two seconds!</h5>
+            </div>
+        )
+    }
 }
 
 export default NotFoundPage;
