8a4d6a90446a1e75e94c8cb8eaa7aa0b7e3b3572
Thomas Barton <verites95@gmail.com>
2020-09-03 16:36:18 +0100

    Commiting to pull

--- src/main/java/com/mthree/bsm/App.java
+++ src/main/java/com/mthree/bsm/App.java
@@ -12,7 +12,7 @@ public class App {
         
         r.addShutdownHook(new Thread(){
             public void run(){  
-                System.out.println("shut down hook task completed..");  
+                System.out.println("shut down hook task completed...");  
              }  
         });  
         
